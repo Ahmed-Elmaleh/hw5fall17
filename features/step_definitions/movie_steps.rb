1d2115201da17e08daddcca94be471b5a0d9abef
Ahmed Elmaleh <ahmed-elmaleh@uiowa.edu>
2017-10-09 23:53:38 +0000

    /***Hw5 Done***/

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -28,7 +28,7 @@ Given /^I am on the RottenPotatoes home page$/ do
    click_on "More about #{title}"
  end
 
- Then /^(?:|I )should see "([^"]*)"$/ do |text|
+ Then /^(?:|I )should see "([^\"]*)"$/ do |text|
     expect(page).to have_content(text)
  end
 
@@ -46,12 +46,8 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
-    # Each returned movie will be a hash representing one row of the movies_table
-    # The keys will be the table headers and the values will be the row contents.
-    # Entries can be directly to the database with ActiveRecord methods
-    # Add the necessary Active Record call(s) to populate the database.
+    Movie.create!(movie)
   end
 end
 
@@ -59,16 +55,49 @@ When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
   # HINT: use String#split to split up the rating_list, then
   # iterate over the ratings and check/uncheck the ratings
   # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
+
+  all('input[type=checkbox]').each do |rating|
+    if rating.checked? then #check if rating checkbox is checked
+        rating.click #uncheck checked boxed
+    end
+  end
+  ratings = arg1.split(', ')
+  ratings.each {|rating| check('ratings_'+rating)}
+  click_button('ratings_submit')
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+  checked_ratings = Set.new
+  arg1.split(', ').each { |rating| checked_ratings.add(rating) }
+  
+  unchecked_ratings = Set.new ['G', 'PG', 'PG-13', 'NC-17', 'R']
+  unchecked_ratings -= checked_ratings
+
+  result = true
+  all("tr").each do |tr|
+    unchecked_ratings.each do |rating|
+      if tr.text.include?(" #{rating} ")
+        result = false
+      end
+    end
+  end
+  expect(result).to be_truthy
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+  allMovies = Movie.all.size
+  (page.all("table tr").count - 1).should == allMovies
 end
 
+When /^I want to sort movies alphabetically$/ do
+  click_on 'Movie Title'
+end
 
+Then /^I should see "(.*?)" before "(.*?)"$/ do |firstMovie, secondMovie|
+  match = /#{firstMovie}.*#{secondMovie}/m =~ page.body
+  expect(match).to be_truthy
+end
 
+When /^I want to sort movies by release date$/ do
+  click_on 'Release Date'
+end
